5b229a83ebcc83ab63b9ab0fae89e2591fd153c0
arafat <Araf020>
2021-11-02 20:37:42 +0600

    db connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,18 @@
 //database connection
+const mongoose = require('mongoose');
+
+
+const connectDatabase = () => {
+
+mongoose.connect(process.env.DB_URI,{useNewUrlparser: true, useUnifiedTopology: true})
+.then((data)=>{
+    console.log("mongodb conncected with server " + data.connection.host);
+
+}).catch(
+    (err) => {
+        console.log(err)
+    }
+);
+}
+
+module.exports = connectDatabase;
